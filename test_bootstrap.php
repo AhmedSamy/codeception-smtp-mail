8404bf7007a69a9d4886bf112f82900308058ac5
ahmedsamy <ahmed.samy.cs@gmail.com>
2015-09-18 23:24:04 +0200

    Adjusted test files

--- test_bootstrap.php
+++ test_bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!($loader = include __DIR__.'/../../../../vendor/autoload.php')) {
+if (!($loader = include __DIR__.'/../../../vendor/autoload.php')) {
     die(<<<EOT
 You need to install the project dependencies using Composer:
 $ wget http://getcomposer.org/composer.phar
@@ -11,5 +11,3 @@ $ phpunit
 EOT
     );
 }
-// Need to load the state machine bundle for tests in that directory to find its own classes.
-$loader->add('StateMachineBundle', __DIR__);
